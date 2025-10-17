c274bba312b70fdea9c571d09424bdcc2be6d278
Shubh Yadav <akhhjj98865@gmail.com>
2025-10-17 20:32:53 +0530

    Added condition for negative input

---
+++ fact.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main ()
+{
+  int num;
+  long long fact=1;
+  printf("Enter any number: \n");
+  scanf("%d",&num);
+  if(num<0)
+  {
+    printf("Factorial of negstive numbers is not defined. ");
+    return 0;
+  }
+  for(int i=1;i<=num;i++)
+  {
+    fact*=i;
+  }
+  printf("The factorial of the number is %lld",fact);
+  return 0;
+}
