007f85d4f5f5356664c2be278f9c396682f0e322
Shubh Yadav <akhhjj98865@gmail.com>
2026-03-16 13:57:08 +0530

    Added multiple new programming langages.

---
+++ BubbleSort.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func bubbleSort(arr []int) {
+	n := len(arr)
+
+	for i := 0; i < n-1; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
+func main() {
+	arr := []int{5, 1, 4, 2, 8}
+	bubbleSort(arr)
+	fmt.Println(arr)
+}
---
+++ BubbleSort.rs
@@ -0,0 +1,19 @@
+fn bubble_sort(arr: &mut [i32]) {
+    let n = arr.len();
+
+    for i in 0..n {
+        for j in 0..n-i-1 {
+            if arr[j] > arr[j+1] {
+                arr.swap(j, j+1);
+            }
+        }
+    }
+}
+
+fn main() {
+    let mut arr = [5, 1, 4, 2, 8];
+
+    bubble_sort(&mut arr);
+
+    println!("{:?}", arr);
+}
